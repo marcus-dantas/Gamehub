3c587d8507e83497203a9a4781113ba55ee09961
Juan Pablo Aponte <juan8419@gmail.com>
2020-02-24 17:57:59 +0000

    unfinished booking controller and views

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,33 @@
+class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+  end
+
+  def show
+    @booking = booking.find(params[:id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @booking = booking.find(params[:id])
+    @booking.update(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+  private
+  def booking_params
+    params.require(:booking).permit()
+  end
+end
