756c581b9b6456a32805f076603ac1c03c4922bf
Juan Pablo Aponte <juan8419@gmail.com>
2020-02-24 15:32:23 +0000

    seeds file under review

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,21 @@
 #
 # Examples:
 #
+puts 'Creating 100 fake games...'
+100.times do
+  game = Game.new(
+    title: Faker::Game.title,
+    category: Faker::Game.genre,
+    description: Faker::Lorem.sentences(number: 2),
+    address: Faker::Address.street_address,
+    daily_price: rand(30..50)
+  )
+  game.save!
+end
+puts 'Finished!'
+
+
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
