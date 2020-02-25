8b7396467bb343467fa8f51aba8d5a185163a564
Juan Pablo Aponte <juan8419@gmail.com>
2020-02-25 10:51:25 +0000

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-<<<<<<< HEAD
-    resources :bookings, only: [:new, :create, :edit]
-
-=======
-  resources :games
->>>>>>> 466ea3c143077a5bdb17f776da419c513032d9af
+    resources :bookings, only: [:new, :create, :edit, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
