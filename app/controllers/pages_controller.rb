a9e73453cfb135a5d8011edf20b9753c8735b457
Kaloyan Atansov <kaloyanatanasov83@gmail.com>
2020-02-25 17:43:29 +0000

    added category filter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,16 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    
+    # if params are there 
+    # then filter with .where(..)
+    # else
+    # just Game.all
+    
+    if params[:category].present?
+      @games = Game.where(category: params[:category])
+    else
     @games = Game.all
+    end
   end
 end
